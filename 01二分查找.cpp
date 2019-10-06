c8913cfd5478012e30438485e6363dc81a7c1725
BJ-RT <38686153+BJ-RT@users.noreply.github.com>
2019-10-06 21:51:58 +0800

    Update 01二分查找.cpp

--- 01二分查找.cpp
+++ 01二分查找.cpp
@@ -1,3 +1,52 @@
+自制：
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void binarysearch(vector<int> input, int search)
+{
+	int low = 0;
+	int high = input.size()-1;
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+		if (input[mid] == search)
+		{
+			cout << "查找成功" << input[mid] << endl;
+			return;
+		}
+		else if (input[mid] > search)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	cout << "未找到" << endl;
+}
+
+int main()
+{
+	vector<int> input;
+	int temp;
+	cout << "输入：" << endl;
+	while (!cin.eof())
+	{
+		cin >> temp;
+		input.push_back(temp);
+	}
+	input.pop_back();
+
+	cin.clear();
+
+	int search;
+		cout << "输入查找的数字：" << endl;
+		cin >> search;
+		binarysearch(input, search);
+	return 0;
+}
+
+
+他人：
 #include "iostream"
 using namespace std;
 
